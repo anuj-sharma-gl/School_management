c98beaf5fdc772c0ba92764465edb58756509f76
anuj <anuj.sharma3@globallogic.com>
2020-05-29 17:39:59 +0530

    Added Eureka Server

---
+++ eureka-server/src/test/java/com/management/school/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.school.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
